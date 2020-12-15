b8115f9b6438c15a985321c2f6134977eafae593
Jacob Coles <jacob@five2one.com.au>
2020-12-15 13:20:35 +0100

    recursion1.py works now

--- tasks/pad.py
+++ tasks/pad.py
@@ -1,15 +1,32 @@
+import numpy
 
+input_list = [[1,2,3], [1,2,3,4,5], [1,2], [1,2,3,4,5,6,7,8]]
 
 def pad(input_list):
-    """
-    Given a input list of nested list of integers:
+	"""
+	Given a input list of nested list of integers:
 
-    input_list = [[1,2,3], [1,2,3,4,5], [1,2], [1,2,3,4,5,6,7,8]]
+	input_list = [[1,2,3], [1,2,3,4,5], [1,2], [1,2,3,4,5,6,7,8]]
 
-    return a matrix where 
-    1) matrix[:len(input_list[i])] == input_list[i]
-    and
-    2) matrix[len(input_list[i]):] == [0,0, ..., 0]
+	return a matrix where 
+	1) matrix[i][:len(input_list[i])] == input_list[i]
+	and
+	2) matrix[i][len(input_list[i]):] == [0,0, ..., 0]
 
-    """
-    pass
+	"""
+	#pass
+	
+	#from the bottom item of 'matrix' up to the length of 'input' item 'i', will be the same as item 'i' in 'input'
+	
+	#numpy.pad(input_list)
+	
+	max_length = len(max(input_list, key=len))
+	
+	matrix = []
+	for item in input_list:
+		extra_zeroes = max_length - len(item)
+		matrix.append(list(numpy.pad(item, (0,extra_zeroes), 'constant')))
+	
+	return matrix
+		
+print(pad(input_list))
